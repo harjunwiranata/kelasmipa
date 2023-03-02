bc2b02ce4c815e84e289a6bb5317775e8feaa40d
mirakeuri <mirakeuri@gmail.com>
2023-03-02 22:19:02 +0700

    feat: upgrade to next 13

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,22 +1,22 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
-  language: 'en-us',
-  theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  title: 'Kelas MIPA',
+  author: 'harjunwiranata',
+  headerTitle: 'Kelas MIPA',
+  description: 'Tempat informasi seputar matematika dan IPA',
+  language: 'id-id',
+  theme: 'dark', // system, dark or light
+  siteUrl: 'https://kelasmipa.com',
+  siteRepo: 'https://github.com/harjunwiranata/kelasmipa',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
   email: 'address@yoursite.com',
   github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
+  twitter: 'https://twitter.com/kelasmipa_id',
   facebook: 'https://facebook.com',
   youtube: 'https://youtube.com',
   linkedin: 'https://www.linkedin.com',
-  locale: 'en-US',
+  locale: 'id-ID',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
